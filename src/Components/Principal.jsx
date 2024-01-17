bbe93df4452dac7d312f96aceaad4ed732f3b8c3
Camilo <Camilogzn19@gmail.com>
2024-01-17 16:58:31 -0500

    adding last features 17 jan

--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -91,78 +91,81 @@ const Principal = () => {
             ))}
           </CardCont>
         </>
+        <>
+          {filtro === "Hogar y cocina" ||
+          filtro === "Todos" ||
+          filtro === undefined ? (
+            <>
+              <HeadLine>Hogar y Cocina</HeadLine>
+              <Sep />
+            </>
+          ) : (
+            <></>
+          )}
+          <CardCont>
+            {product.slice(4, 8).map((e) => (
+              <Card key={e.id}>
+                <Link style={{ color: "inherit" }} to="/product">
+                  <Image
+                    src={e.image1}
+                    alt="product"
+                    id={e.id}
+                    onClick={(e) => Detallar(e)}
+                  />
+                </Link>
+                <Til>{e.snombre}</Til>
+                <BodyBold1>COP$ {e.precio}</BodyBold1>
+                <Body2R>
+                  <b>Envío GRATIS</b> a Colombia cuando gastes más de USD 35.00
+                  (COP 134,539.47) en artículos elegibles
+                </Body2R>
+                <BtnComprar id={e.id} onClick={(e) => Comprar(e)}>
+                  Añadir al carrito
+                </BtnComprar>
 
-        {filtro === "Hogar y cocina" ||
-        filtro === "Todos" ||
-        filtro === undefined ? (
-          <>
-            <HeadLine>Hogar y Cocina</HeadLine>
-            <Sep />
-          </>
-        ) : (
-          <></>
-        )}
-        <CardCont>
-          {product.slice(4, 8).map((e) => (
-            <Card key={e.id}>
-              <Link style={{ color: "inherit" }} to="/product">
-                <Image
-                  src={e.image1}
-                  alt="product"
-                  id={e.id}
-                  onClick={(e) => Detallar(e)}
-                />
-              </Link>
-              <Til>{e.snombre}</Til>
-              <BodyBold1>COP$ {e.precio}</BodyBold1>
-              <Body2R>
-                <b>Envío GRATIS</b> a Colombia cuando gastes más de USD 35.00
-                (COP 134,539.47) en artículos elegibles
-              </Body2R>
-              <BtnComprar id={e.id} onClick={(e) => Comprar(e)}>
-                Añadir al carrito
-              </BtnComprar>
+                <br />
+              </Card>
+            ))}
+          </CardCont>
+        </>
+        <>
+          {filtro === "Artículos para mascotas" ||
+          filtro === "Todos" ||
+          filtro === undefined ? (
+            <>
+              <HeadLine>Artículos para mascotas</HeadLine>
+              <Sep />
+            </>
+          ) : (
+            <></>
+          )}
 
-              <br />
-            </Card>
-          ))}
-        </CardCont>
-        {filtro === "Artículos para mascotas" ||
-        filtro === "Todos" ||
-        filtro === undefined ? (
-          <>
-            <HeadLine>Artículos para mascotas</HeadLine>
-            <Sep />
-          </>
-        ) : (
-          <></>
-        )}
+          <CardCont>
+            {product.slice(8, 12).map((e) => (  
+              <Card key={e.id}>
+                <Link style={{ color: "inherit" }} to="/product">
+                  <Image
+                    src={e.image1}
+                    alt="product"
+                    id={e.id}
+                    onClick={(e) => Detallar(e)}
+                  />
+                </Link>
+                <Til>{e.snombre}</Til>
+                <BodyBold1>COP$ {e.precio}</BodyBold1>
+                <Body2R>
+                  <b>Envío GRATIS</b> a Colombia cuando gastes más de USD 35.00
+                  (COP 134,539.47) en artículos elegibles
+                </Body2R>
+                <BtnComprar id={e.id} onClick={(e) => Comprar(e)}>
+                  Añadir al carrito
+                </BtnComprar>
 
-        <CardCont>
-          {product.slice(8, 12).map((e) => (
-            <Card key={e.id}>
-              <Link style={{ color: "inherit" }} to="/product">
-                <Image
-                  src={e.image1}
-                  alt="product"
-                  id={e.id}
-                  onClick={(e) => Detallar(e)}
-                />
-              </Link>
-              <Til>{e.snombre}</Til>
-              <BodyBold1>COP$ {e.precio}</BodyBold1>
-              <Body2R>
-                <b>Envío GRATIS</b> a Colombia cuando gastes más de USD 35.00
-                (COP 134,539.47) en artículos elegibles
-              </Body2R>
-              <BtnComprar id={e.id} onClick={(e) => Comprar(e)}>
-                Añadir al carrito
-              </BtnComprar>
-
-              <br />
-            </Card>
-          ))}
-        </CardCont>
+                <br />
+              </Card>
+            ))}
+          </CardCont>
+        </>
       </Cont>
     </div>
   );
